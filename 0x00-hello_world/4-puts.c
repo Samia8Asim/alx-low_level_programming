b84b1a105a9824cdf3f694f15e6cd1b5b2f6f653
Samia8Asim <samia2003.8@gmail.com>
2023-09-07 11:56:21 -0700

    committing some code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : A c programm that print using puts function 
+ *
+ * Return alawys 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
