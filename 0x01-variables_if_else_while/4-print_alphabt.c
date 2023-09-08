7f3db4fb1c1dc685f146f3dd3e8a3e20fdcaedf1
Samia8Asim <samia2003.8@gmail.com>
2023-09-08 13:46:51 -0700

    committing some code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that use putchar function
+ *
+ * Return: Alawys 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
