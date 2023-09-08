27963dcac2d5de1b958e32541ffed3384d7d26a3
Samia8Asim <samia2003.8@gmail.com>
2023-09-08 04:21:02 -0700

    committing some code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,14 @@ int main(void)
 	char c1 = 'a';
 	char c2 = 'A';
 
-	while (c1 <= 'z' && c2 <= 'Z')
+	while (c1 <= 'z')
 	{
 		putchar(c1);
-		putchar(c2);
 		c1++;
+	}
+	while (c2 <= 'Z')
+	{
+		putchar(c2);
 		c2++;
 	}
 	putchar('\n');
