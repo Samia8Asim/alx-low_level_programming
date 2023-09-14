80fa51ae99b3005695f2808f32c47a32bb602afd
Samia8Asim <samia2003.8@gmail.com>
2023-09-13 22:56:02 -0700

    committing some code

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers,from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int r, c, n;
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			n = c;
+
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
