0f39f5e8a0d93c9fee9e4e9f3538e8ee7bc72868
Samia8Asim <samia2003.8@gmail.com>
2023-09-18 01:53:53 -0700

    committing some code

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: input string pointer
+ */
+
+void puts2(char *str)
+{
+	int count = 0, i;
+
+	while (str[count])
+		count++;
+	for (i = 0; i < count; i++)
+	{
+		if (i % 2 != 0 || i == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
