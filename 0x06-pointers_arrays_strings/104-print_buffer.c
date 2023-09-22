2dda32a9526f0ff26b0e870f01a0b70a1d39ef1a
Samia8Asim <samia2003.8@gmail.com>
2023-09-22 13:24:36 -0700

    committing some code

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,7 +3,7 @@
 /**
  * isAscii - determines if ch is ascii character
  *
- * @ch: input character
+ * @n: input character
  *
  * Return: 1 is true ,0 if false
  */
@@ -23,10 +23,64 @@ int isAscii(int n)
 
 void printHexa(char *a, int start, int end)
 {
-	int i =0;
+	int i = 0;
 
 	while (i < 10)
 	{
-		
+		if (i < end)
+		printf("%02x", *(a + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
 	}
+}
+
+/**
+ * printAscii - print ascii value
+ *
+ * @c: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+
+void printAscii(char *c, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(c + start + i);
+		if (isAscii(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer -  function that prints a buffer.
+ *
+ * @b: string input
+ * @size: size of buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexa(b, start, end);
+			printAscii(b, start, end);
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
 }
