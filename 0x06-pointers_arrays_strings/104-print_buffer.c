c2b714b5c6be25b89c52d841cdbbb22edd9333ec
Samia8Asim <samia2003.8@gmail.com>
2023-09-22 06:57:15 -0700

    committing some code

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,32 @@
+# include "main.h"
+
+/**
+ * isAscii - determines if ch is ascii character
+ *
+ * @ch: input character
+ *
+ * Return: 1 is true ,0 if false
+ */
+
+int isAscii(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexa - print hexa valuse of string
+ *
+ * @a: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+
+void printHexa(char *a, int start, int end)
+{
+	int i =0;
+
+	while (i < 10)
+	{
+		
+	}
+}
