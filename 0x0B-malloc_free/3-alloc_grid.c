c8c9bb4d99659c9b247a745acbe326409d1c5d6c
Samia8Asim <samia2003.8@gmail.com>
2023-10-05 01:05:55 -0700

    committing some code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,7 @@ int **alloc_grid(int width, int height)
 	else
 	{
 		for (i = 0; i < height; i++)
+		{
 			grid[i] = malloc(sizeof(**grid) * width);
 			if (grid[i] == 0)
 			{
@@ -30,8 +31,7 @@ int **alloc_grid(int width, int height)
 				free(grid);
 				return (NULL);
 			}
-		for (; i > 0; i--)
-		{
+
 			for (j = 0; j < width; j++)
 			{
 				grid[i][j] = 0;
