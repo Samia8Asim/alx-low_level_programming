7bdca58c348e7c979995bfec64cc14be37290bb9
Samia8Asim <samia2003.8@gmail.com>
2023-10-07 22:39:28 -0700

    committing some code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,23 +12,22 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2;
+	unsigned int len1 = n, i, j, len2;
 	char *sp;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	for (len2 = 0; s1[len2] != '\0'; len1++)
+		len1++;
 	sp = malloc(sizeof(char) * (1 + len1));
 
 	if (sp == NULL)
 	{
 		return (NULL);
 	}
+	len1 = 0;
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		sp[len1++] = s1[i];
