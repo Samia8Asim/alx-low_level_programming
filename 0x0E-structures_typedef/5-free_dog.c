c2f1d91be6c2599dca8b1be71f92511bc4461681
Samia8Asim <samia2003.8@gmail.com>
2023-10-10 12:26:15 -0700

    committing some code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+nclude "dog.h"
+#include <stdlib.h>
+
+/***
+ * free_dog -  function that frees dogs.
+ * @d: dog struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
