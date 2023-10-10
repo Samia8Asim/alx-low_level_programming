81e785fde2abf736bacc0b60ac94fd32eaac4a06
Samia8Asim <samia2003.8@gmail.com>
2023-10-10 11:39:08 -0700

    committing some code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct name dog
+ * @name: dog name
+ * @age : dog age
+ * @owner: dog owner
+ *
+ * Description: dog information
+ */
+
 struct dog 
 {
 	char *name;
