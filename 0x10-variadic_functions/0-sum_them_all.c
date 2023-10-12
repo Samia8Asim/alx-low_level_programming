8e2ce8fd2b0654eb605adb2f2c8f24b85098bc6c
Samia8Asim <samia2003.8@gmail.com>
2023-10-11 23:38:10 -0700

    committing some code

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of its parameters
+ * @n: argument number 
+ *
+ * Return: the sum of its parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list args;
+
+	if (!n)
+	{
+		return (0);
+	}
+	va_start(args, n);
+	for (i = 0, i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (args);
+}
