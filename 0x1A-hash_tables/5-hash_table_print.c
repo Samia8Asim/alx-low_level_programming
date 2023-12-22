d91bb5a5f6ef70b31722af3597a7dea48a04b22d
Samia8Asim <samia2003.8@gmail.com>
2023-12-22 06:59:59 +0200

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,6 +8,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *curr;
 	unsigned long int i;
+	int fp = 1;
 
 	if (!(ht))
 		return;
@@ -18,10 +19,11 @@ void hash_table_print(const hash_table_t *ht)
 		curr = ht->array[i];
 		while (curr != NULL)
 		{
-			printf("'%s': '%s'", curr->key, curr->value);
-			if (curr->next != NULL)
+			if (!(fp))
 				printf(", ");
+			printf("'%s': '%s'", curr->key, curr->value);
 			curr = curr->next;
+			fp = 0;
 		}
 	}
 	printf("}\n");
